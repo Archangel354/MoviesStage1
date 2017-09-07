f99eb8d51de15464e64c9eae68179c9db3e17a80
Archangel354 <rduncan354@yahoo.com>
2017-09-07 13:45:36 -0400

    added detail.xml and forecast.xml

--- app/src/main/java/com/example/android/moviesstage1/DetailActivity.java
+++ app/src/main/java/com/example/android/moviesstage1/DetailActivity.java
@@ -1,13 +1,51 @@
 package com.example.android.moviesstage1;
 
+import android.content.Intent;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
+
+    private static final String MOVIES_SHARE_HASHTAG = " #MoviesStage1App";
+
+    private String mMovies;
+    private TextView mMovieDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        mMovieDisplay = (TextView) findViewById(R.id.txtMovies);
+
+        Intent intentThatStartedThisActivity = getIntent();
+
+        if (intentThatStartedThisActivity != null) {
+            if (intentThatStartedThisActivity.hasExtra(Intent.EXTRA_TEXT)) {
+                mMovies = intentThatStartedThisActivity.getStringExtra(Intent.EXTRA_TEXT);
+                mMovieDisplay.setText(mMovies);
+            }
+        }
+
+    }private Intent createShareForecastIntent() {
+        Intent shareIntent = ShareCompat.IntentBuilder.from(this)
+                .setType("text/plain")
+                .setText(mMovies + MOVIES_SHARE_HASHTAG)
+                .getIntent();
+        return shareIntent;
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.detail, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        menuItem.setIntent(createShareForecastIntent());
+        return true;
+    }
+
+
 }

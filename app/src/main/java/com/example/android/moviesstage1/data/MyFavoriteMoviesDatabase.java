c50d2532b1325eed5f7ee2e13220756fec72b4bc
Archangel354 <rduncan354@gmail.com>
2018-10-21 13:42:53 -0400

    7th commit: added MyFavoriteMovies.java and added liveData dependencies to the app build.gradle

---
+++ app/src/main/java/com/example/android/moviesstage1/data/MyFavoriteMoviesDatabase.java
@@ -0,0 +1,19 @@
+package com.example.android.moviesstage1.data;
+
+import android.arch.persistence.db.SupportSQLiteOpenHelper;
+import android.arch.persistence.room.DatabaseConfiguration;
+import android.arch.persistence.room.InvalidationTracker;
+import android.arch.persistence.room.RoomDatabase;
+
+
+public abstract class MyFavoriteMoviesDatabase extends RoomDatabase {
+    @Override
+    protected SupportSQLiteOpenHelper createOpenHelper(DatabaseConfiguration config) {
+        return null;
+    }
+
+    @Override
+    protected InvalidationTracker createInvalidationTracker() {
+        return null;
+    }
+}
